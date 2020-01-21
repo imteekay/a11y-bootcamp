452b1b6049ca543c92a3cc1dc001d10a04f1f3c2
TK <leandrotk100@gmail.com>
2020-01-21 09:43:00 -0300

    Use named import

--- src/App.js
+++ src/App.js
@@ -14,48 +14,44 @@ import { Image } from './components/Image';
 import { Aria } from './components/Aria';
 import { Form } from './components/Form';
 
-function App() {
-  return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/semantic-html">Semantic HTML</Link>
-          </li>
-          <li>
-            <Link to="/image">Image</Link>
-          </li>
-          <li>
-            <Link to="/aria">Aria</Link>
-          </li>
-          <li>
-            <Link to="/form">Form</Link>
-          </li>
-        </ul>
-      </nav>
+export const App = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/semantic-html">Semantic HTML</Link>
+        </li>
+        <li>
+          <Link to="/image">Image</Link>
+        </li>
+        <li>
+          <Link to="/aria">Aria</Link>
+        </li>
+        <li>
+          <Link to="/form">Form</Link>
+        </li>
+      </ul>
+    </nav>
 
-      <Switch>
-        <Route path="/semantic-html">
-          <SemanticHTML />
-        </Route>
-        <Route path="/image">
-          <Image />
-        </Route>
-        <Route path="/aria">
-          <Aria />
-        </Route>
-        <Route path="/form">
-          <Form />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
-
-export default App;
+    <Switch>
+      <Route path="/semantic-html">
+        <SemanticHTML />
+      </Route>
+      <Route path="/image">
+        <Image />
+      </Route>
+      <Route path="/aria">
+        <Aria />
+      </Route>
+      <Route path="/form">
+        <Form />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+);
