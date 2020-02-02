e39f8d822cc8ec398fe01b4a459eca741acc6235
TK <leandrotk100@gmail.com>
2020-02-02 15:30:54 -0300

    Improved semantic web component

--- src/components/SemanticWeb.js
+++ src/components/SemanticWeb.js
@@ -1,10 +1,31 @@
 import React, { Fragment } from 'react';
-import { SemanticHTML } from './SemanticWeb/SemanticHTML';
 
 export const SemanticWeb = () => (
   <Fragment>
     <h1>Semantic Web</h1>
 
-    <SemanticHTML />
+    <h1>Semantic HTML</h1>
+
+    <font size="7">My heading</font>
+
+    <br></br>
+    <br></br>
+
+    This is the first section of my document.
+
+    <div>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </div>
+
+    <font size="5">My subheading</font>
+
+    <br></br>
+    <br></br>
+
+    This is the first subsection of my document. I'd love people to be able to find this content!
+
+    <div className="button-ish">Capture Pikachu!</div>
   </Fragment>
 );
