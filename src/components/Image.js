93525f4344fc832d75e42e5d9e7f9ad987b8d96f
TK <leandrotk100@gmail.com>
2020-02-02 16:51:45 -0300

    Fix image alt

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,6 +4,6 @@ import pikachu from '../images/pikachu.png';
 export const Image = () => (
   <Fragment>
     <h1>Image</h1>
-    <img src={pikachu} width="300" />
+    <img src={pikachu} width="300" alt="A suprised Pikachu" />
   </Fragment>
 );
