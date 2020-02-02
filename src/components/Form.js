c3b3fcb14735a651f0dce61c0d859591342f30ac
TK <leandrotk100@gmail.com>
2020-02-02 17:27:58 -0300

    Fix form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,44 +4,40 @@ export const Form = () => (
   <Fragment>
     <h1>Form</h1>
 
-    <div>
-      <label>Nome:</label>
-      <input id="name" name="name" placeholder="Ex: TK" />
-    </div>
+    <label htmlFor="name">Nome:</label>
+    <input id="name" name="name" placeholder="Ex: TK" />
+
+    <label htmlFor="age">Idade:</label>
+    <input id="age" name="age" type="number" />
+
+    <fieldset>
+      <legend>Hobbies:</legend>
+
+      <input type="checkbox" name="hobbie" id="hobbie1" value="coding" />
+      <label htmlFor="hobbie1">Coding</label>
+
+      <input type="checkbox" name="hobbie" id="hobbie2" value="reading" />
+      <label htmlFor="hobbie2">Reading</label>
+
+      <input type="checkbox" name="hobbie" id="hobbie3" value="writing" />
+      <label htmlFor="hobbie3">Writing</label>
+    </fieldset>
+
+    <fieldset>
+      <legend>OS:</legend>
+
+      <input type="radio" name="os" id="os1" value="windows" />
+      <label htmlFor="os1">Windows</label>
+
+      <input type="radio" name="os" id="os2" value="mac" />
+      <label htmlFor="os2">Mac</label>
+
+      <input type="radio" name="os" id="os3" value="linux" />
+      <label htmlFor="os3">Linux</label>
+    </fieldset>
 
     <div>
-      <label>Idade:</label>
-      <input id="age" name="age" type="number" />
-    </div>
-
-    <div>
-      Hobbies:
-
-      <input type="checkbox" name="hobbie1" id="hobbie1" value="coding" />
-      <label>Coding</label>
-
-      <input type="checkbox" name="hobbie2" id="hobbie2" value="reading" />
-      <label>Reading</label>
-
-      <input type="checkbox" name="hobbie3" id="hobbie3" value="writing" />
-      <label>Writing</label>
-    </div>
-
-    <div>
-      OS:
-
-      <input type="radio" name="os1" id="os1" value="windows" />
-      <label>Windows</label>
-
-      <input type="radio" name="os2" id="os2" value="mac" />
-      <label>Mac</label>
-
-      <input type="radio" name="os3" id="os3" value="linux" />
-      <label>Linux</label>
-    </div>
-
-    <div>
-      <label>Favorite city?</label>
+      <label htmlFor="favcity">Favorite city?</label>
       <select id="favcity" name="select">
         <option value="1">Amsterdam</option>
         <option value="2">Hong Kong</option>
